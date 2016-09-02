eedb347d578978e55b66b3d45336907f97d1f7aa
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2016-09-02 11:46:23 +0200

    Get requested URI.

--- src/main/java/com/ipfssearch/ipfstika/App.java
+++ src/main/java/com/ipfssearch/ipfstika/App.java
@@ -23,13 +23,9 @@ public class App extends NanoHTTPD {
 
     @Override
     public Response serve(IHTTPSession session) {
-        String msg = "<html><body><h1>Hello server</h1>\n";
-        Map<String, String> parms = session.getParms();
-        if (parms.get("username") == null) {
-            msg += "<form action='?' method='get'>\n  <p>Your name: <input type='text' name='username'></p>\n" + "</form>\n";
-        } else {
-            msg += "<p>Hello, " + parms.get("username") + "!</p>";
-        }
-        return newFixedLengthResponse(msg + "</body></html>\n");
+        String uri = session.getUri();
+        String hash = uri.substring(1);
+
+        return newFixedLengthResponse(hash);
     }
 }
