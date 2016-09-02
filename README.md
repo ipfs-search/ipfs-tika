8b306c5cc3a88e1a17acea4535ecb2d976aacb48
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2016-09-02 11:28:51 +0200

    Nano webserver running \o/

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # ipfs-tika
 Java web application taking IPFS hashes, extracting (textual) content and metadata through Apache's Tika.
 
-## Compiling & running
-`mvn package && java -cp target/ipfs-tika-1.0-SNAPSHOT.jar com.ipfssearch.ipfstika.App`
+## Compiling
+`mvn compile`
+
+## Running
+`mvn exec:java -Dexec.mainClass="com.ipfssearch.ipfstika.App"`
--- src/main/java/com/ipfssearch/ipfstika/App.java
+++ src/main/java/com/ipfssearch/ipfstika/App.java
@@ -1,13 +1,35 @@
 package com.ipfssearch.ipfstika;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.IOException;
+import java.util.Map;
+
+import fi.iki.elonen.NanoHTTPD;
+
+public class App extends NanoHTTPD {
+
+    public App() throws IOException {
+        super(8081);
+        start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+        System.out.println("\nRunning! Point your browsers to http://localhost:8081/ \n");
+    }
+
+    public static void main(String[] args) {
+        try {
+            new App();
+        } catch (IOException ioe) {
+            System.err.println("Couldn't start server:\n" + ioe);
+        }
+    }
+
+    @Override
+    public Response serve(IHTTPSession session) {
+        String msg = "<html><body><h1>Hello server</h1>\n";
+        Map<String, String> parms = session.getParms();
+        if (parms.get("username") == null) {
+            msg += "<form action='?' method='get'>\n  <p>Your name: <input type='text' name='username'></p>\n" + "</form>\n";
+        } else {
+            msg += "<p>Hello, " + parms.get("username") + "!</p>";
+        }
+        return newFixedLengthResponse(msg + "</body></html>\n");
     }
 }
