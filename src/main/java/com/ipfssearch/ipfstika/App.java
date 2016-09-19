30557402cfdc658a0b67d7fb71657b46c1f57a49
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2016-09-19 12:18:47 +0200

    Links as URLs to not clash with existing metadata.

--- src/main/java/com/ipfssearch/ipfstika/App.java
+++ src/main/java/com/ipfssearch/ipfstika/App.java
@@ -150,7 +150,7 @@ public class App extends NanoHTTPD {
         Gson gson = new Gson();
         JsonObject output_json = gson.toJsonTree(metadata).getAsJsonObject();
         output_json.add("content", gson.toJsonTree(body_handler.toString().trim()));
-        output_json.add("links", gson.toJsonTree(links));
+        output_json.add("urls", gson.toJsonTree(links));
 
         return output_json.toString();
     }
