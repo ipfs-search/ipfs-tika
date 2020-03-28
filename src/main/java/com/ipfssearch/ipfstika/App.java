64b7243a1e6896ff604d58abd1ede7117face2dc
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-03-28 12:47:48 +0000

    Update dependencies, fix moved import, bump vers.

--- src/main/java/com/ipfssearch/ipfstika/App.java
+++ src/main/java/com/ipfssearch/ipfstika/App.java
@@ -15,7 +15,7 @@ import java.util.Properties;
 
 import fi.iki.elonen.NanoHTTPD;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import org.apache.http.client.utils.URIBuilder;
 
